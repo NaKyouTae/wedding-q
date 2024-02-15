90a33c08fdda92a4e9251271ef757a0e367e8718
nakyoutae <qppk@naver.com>
2024-02-15 21:58:14 +0900

    first react

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wedding Kyoutae Learn React
         </a>
       </header>
     </div>
